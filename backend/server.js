c3166c3bed99a0ae14ee6bb08030feb51fe44e9c
sanketpawar4972 <sp6565837@gmail.com>
2025-11-28 20:30:44 +0530

    Fix Render wildcard route + set module type

--- backend/server.js
+++ backend/server.js
@@ -39,9 +39,11 @@ if (process.env.NODE_ENV === "production") {
   // Serve frontend build folder
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
-  });
+  app.get("(.*)", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+});
+
+
 } else {
   // Development mode helper
   app.get("/", (req, res) => {
